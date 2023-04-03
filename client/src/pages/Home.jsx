030ea1d37fb4ac9625488a5f0c2cc862196c248a
Ahad-Anjum <aanjum@macalester.edu>
2023-04-03 08:57:31 -0500

    Setup complete

---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,18 @@
+import {motion, AnimatePresence } from 'framer-motion';
+import { useSnapshot } from 'valtio';
+import {
+  headContainerAnimation,
+  headContentAnimation,
+  headTextAnimation,
+  slideAnimation
+} from '..config/motion';
+
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>Home</div>
+  )
+}
+
+export default Home
