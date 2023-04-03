62e74fec0c7b8179d2ee83cd2c3b0ec436d7dda1
Ahad-Anjum <aanjum@macalester.edu>
2023-04-03 16:20:46 -0500

    3D canvas - need to fix so that shirt is showing

---
+++ client/src/canvas/backdrop.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const backdrop = () => {
+  return (
+    <div>backdrop</div>
+  )
+}
+
+export default backdrop
--- client/src/canvas/index.jsx
+++ client/src/canvas/index.jsx
@@ -1,8 +1,23 @@
-import React from 'react'
+import {Canvas} from '@react-three/fiber';
+import {Environment, Center} from '@react-three/drei';
+import Shirt from './Shirt';
+import backdrop from './backdrop';
+import CameraRig from './CameraRig';
 
-const Canvas = () => {
+
+const CanvasModel = () => {
   return (
-    <div>Canvas</div>
+    <Canvas>
+      <ambientLight intensity={0.5}/>
+      <Environment preset='city'/>
+      
+      {/*<CameraRig>*/}
+        {/*<backdrop />*/}
+        <center>
+          <Shirt/>
+        </center>
+      {/*<CameraRig>*/}
+    </Canvas>
   )
 }
 
