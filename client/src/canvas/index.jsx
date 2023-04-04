497b63630dbaba63b1e7a54f981aa476d9d887db
Ahad-Anjum <aanjum@macalester.edu>
2023-04-04 00:27:18 -0500

    3JS Camera is done

--- client/src/canvas/index.jsx
+++ client/src/canvas/index.jsx
@@ -1,5 +1,6 @@
 import {Canvas} from '@react-three/fiber';
 import {Environment, Center} from '@react-three/drei';
+
 import Shirt from './Shirt';
 import backdrop from './backdrop';
 import CameraRig from './CameraRig';
@@ -8,17 +9,17 @@ import CameraRig from './CameraRig';
 const CanvasModel = () => {
   return (
     <Canvas>
-      <ambientLight intensity={0.5}/>
-      <Environment preset='city'/>
+      <ambientLight intensity={0.5} />
+      <Environment preset='city' />
       
-      {/*<CameraRig>*/}
-        {/*<backdrop />*/}
-        <center>
-          <Shirt/>
-        </center>
-      {/*<CameraRig>*/}
+      {<CameraRig/>}
+        {/* <backdrop /> */}
+        <Center>
+          <Shirt />
+        </Center>
+      {<CameraRig/>}
     </Canvas>
   )
 }
 
-export default Canvas
+export default CanvasModel
